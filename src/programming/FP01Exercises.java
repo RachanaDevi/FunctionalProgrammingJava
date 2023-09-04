8f3870aa99fda43a2fb02dafb9ebdc5d1bf805ff
Rachana Devi <rachanadevi18@gmail.com>
2023-09-04 20:50:56 +0530

    Rachana | FP01 | Add functional way and pass method reference to print the numbers

--- src/programming/FP01Exercises.java
+++ src/programming/FP01Exercises.java
@@ -5,7 +5,20 @@ import java.util.List;
 public class FP01Exercises {
 
     public static void main(String[] args) {
-        printAllNumbersInStructured(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        printAllNumbersInFunctionalWay(numbers);
+        printAllNumbersInStructured(numbers);
+    }
+
+    private static void printAllNumbersInFunctionalWay(List<Integer> numbers) {
+        // create a stream of numbers
+        numbers.stream()
+                // we don't want it to immediately execute, so we pass method reference
+                .forEach(FP01Exercises::print); // Method reference
+    }
+
+    private static void print(int number){
+        System.out.println(number);
     }
 
     private static void printAllNumbersInStructured(List<Integer> numbers) {
