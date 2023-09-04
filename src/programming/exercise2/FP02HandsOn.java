8e2ef9ea6c1282294abc1367d4d3a89563250aba
Rachana Devi <rachanadevi18@gmail.com>
2023-09-04 22:41:11 +0530

    Rachana | FP02 |  Update functionality of reduce to use method reference and show that it can also be done using lamda expression

--- src/programming/exercise2/FP02HandsOn.java
+++ src/programming/exercise2/FP02HandsOn.java
@@ -18,12 +18,29 @@ public class FP02HandsOn {
                 // Stream of number -> give one result
                 // initial value / identity value is 0
                 // accumulation function
-                .reduce(0, FP02HandsOn::sum);
+//                .reduce(0, FP02HandsOn::sum);
+//                .reduce(0, (a, b) -> a + b);// lambda expression
+                .reduce(0, Integer::sum);// Method reference
         // reduction operations parallelize
     }
 
-    private static int sum(int a, int b) {
-        return a + b;
+    /*
+    // so that's how it works internally
+      aggregate, nextNumber
+       0 + 12
+       12 + 9
+       21 + 13
+       34 + 4
+       38 + 6
+       44 + 2
+       46 + 4
+       50 + 12
+       62 + 15
+
+     */
+    private static int sum(int aggregate, int nextNumber) {
+        System.out.println(aggregate + " + " + nextNumber);
+        return aggregate + nextNumber;
     }
 
     private static int addListStructured(List<Integer> numbers) {
