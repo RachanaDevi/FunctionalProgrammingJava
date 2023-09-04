f47fd0828bdb68b049259700847f622b627d113f
Rachana Devi <rachanadevi18@gmail.com>
2023-09-04 21:26:38 +0530

    Rachana | FP01 | Rename class as FP01HandsOn

--- src/programming/FP01HandsOn.java
+++ src/programming/FP01HandsOn.java
@@ -2,7 +2,7 @@ package programming;
 
 import java.util.List;
 
-public class FP01Exercises {
+public class FP01HandsOn {
 
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
