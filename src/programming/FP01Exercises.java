f25141cacd48eb4eb3d5854a579d92ac05ed4cd5
Rachana Devi <rachanadevi18@gmail.com>
2023-09-04 21:04:11 +0530

    Rachana | FP01 | Add functional way to print even numbers

--- src/programming/FP01Exercises.java
+++ src/programming/FP01Exercises.java
@@ -6,8 +6,11 @@ public class FP01Exercises {
 
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-        printAllNumbersInFunctionalWay(numbers);
-        printAllNumbersInStructured(numbers);
+//        printAllNumbersInFunctionalWay(numbers);
+
+//        printAllNumbersInStructured(numbers);
+//        printAllEvenNumbersInStructured(numbers);
+        printAllEvenNumbersInFunctionalWay(numbers);
     }
 
     private static void printAllNumbersInFunctionalWay(List<Integer> numbers) {
@@ -17,9 +20,29 @@ public class FP01Exercises {
                 .forEach(System.out::println); // Method reference
     }
 
+    private static void printAllEvenNumbersInFunctionalWay(List<Integer> numbers) {
+        // create a stream of numbers
+        numbers.stream()
+                // filter if the number is even or not
+                .filter(FP01Exercises::isEven) // Method reference, this is a number to be called in each numbers
+                // we don't want it to immediately execute, so we pass method reference
+                .forEach(System.out::println); // Method reference
+    }
+
     private static void printAllNumbersInStructured(List<Integer> numbers) {
         for (Integer number : numbers) {
             System.out.println(number);
         }
+    }
+
+    private static void printAllEvenNumbersInStructured(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (isEven(number))
+                System.out.println(number);
+        }
+    }
+
+    private static boolean isEven(Integer number) {
+        return number % 2 == 0;
     }
 }
