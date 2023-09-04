1ad77c4d5ec455ad082a870afad4b0406d044dab
Rachana Devi <rachanadevi18@gmail.com>
2023-09-04 22:34:22 +0530

    Rachana | FP02 | Use reduce

--- src/programming/exercise2/FP02HandsOn.java
+++ src/programming/exercise2/FP02HandsOn.java
@@ -8,7 +8,22 @@ public class FP02HandsOn {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 
         int sum = addListStructured(numbers);
+        int sumFunctional = addListFunctionalWay(numbers);
         System.out.println("Sum: " + sum);
+        System.out.println("Sum Functional: " + sumFunctional);
+    }
+
+    private static int addListFunctionalWay(List<Integer> numbers) {
+        return numbers.stream()
+                // Stream of number -> give one result
+                // initial value / identity value is 0
+                // accumulation function
+                .reduce(0, FP02HandsOn::sum);
+        // reduction operations parallelize
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
     }
 
     private static int addListStructured(List<Integer> numbers) {
