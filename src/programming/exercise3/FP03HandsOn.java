a687e52af07900d9ded299f4f282c1b6a0ba51af
Rachana Devi <rachanadevi18@gmail.com>
2023-09-08 23:37:35 +0530

    Rachana | FP03 | Functional interfaces like predicate, function and consumer

---
+++ src/programming/exercise3/FP03HandsOn.java
@@ -0,0 +1,48 @@
+package programming.exercise3;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static programming.common.Inputs.numbers;
+
+public class FP03HandsOn {
+
+    public static void main(String[] args) {
+        // all these are functional interfaces. has only one abstract method
+        Predicate<Integer> isEvenPredicate = number -> number % 2 == 0;
+
+        // a predicate is used to test something and returns a boolean value
+        Predicate<Integer> isEvenPredicate2 = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer number) {
+                return number % 2 == 0;
+            }
+        };
+
+        // accepts one argument and returns the value
+        Function<Integer, Integer> squareFunction = number -> number * number;
+        Function<Integer, Integer> squareFunction2 = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer number) {
+                return number * number;
+            }
+        };
+
+
+        // consumes whatever it gets and does not return anything back
+        Consumer<Integer> printConsumer = System.out::println;
+        Consumer<Integer> printConsumer2 = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+
+
+        numbers.stream()
+                .filter(isEvenPredicate2)
+                .map(squareFunction2)
+                .forEach(printConsumer2);
+    }
+}
