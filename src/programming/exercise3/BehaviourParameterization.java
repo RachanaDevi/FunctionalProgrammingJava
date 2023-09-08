8d6d9d0c5612421c37f584e8fe2bf9b13b950d10
Rachana Devi <rachanadevi18@gmail.com>
2023-09-08 23:53:24 +0530

    Rachana | FP03 | Rename method to filterAndPrint and pass List of numbers

--- src/programming/exercise3/BehaviourParameterization.java
+++ src/programming/exercise3/BehaviourParameterization.java
@@ -1,5 +1,6 @@
 package programming.exercise3;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import static programming.common.Inputs.numbers;
@@ -7,15 +8,14 @@ import static programming.common.Inputs.numbers;
 public class BehaviourParameterization {
 
     public static void main(String[] args) {
-        Predicate<Integer> evenPredicate = number -> number % 2 == 0;
-        Predicate<Integer> oddPredicate = number -> number % 2 != 0;
+        filterAndPrint(numbers, number -> number % 2 == 0);
 
-        printNumbers(evenPredicate);
-
-        printNumbers(oddPredicate);
+        filterAndPrint(numbers, number -> number % 2 != 0);
+        // you're passing the logic of the method as a parameter which is known as
+        // behaviour parameterization
     }
 
-    private static void printNumbers(Predicate<Integer> evenPredicate) {
+    private static void filterAndPrint(List<Integer> numbers, Predicate<Integer> evenPredicate) {
         numbers.stream()
                 .filter(evenPredicate)
                 .forEach(System.out::println);
