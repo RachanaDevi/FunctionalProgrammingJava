b4d590c915a7b3e849a9499fe11db237b67a45a0
Rachana Devi <rachanadevi18@gmail.com>
2023-09-09 19:18:05 +0530

    Rachana | FP04 | Use comparators with ascending and descending order according to noOfStudents and reviewScore of courses

--- src/programming/exercise4/FP04HandsOn.java
+++ src/programming/exercise4/FP04HandsOn.java
@@ -1,7 +1,10 @@
 package programming.exercise4;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class FP04HandsOn {
     record Course(String name, String category, int reviewScore, int noOfStudents) {
@@ -30,6 +33,25 @@ public class FP04HandsOn {
 
         // anyMatch
         System.out.println(courses.stream().anyMatch(reviewScoreGreaterThanNinety()));
+
+        // sort courses by number of students
+        Comparator<Course> comparingByNoOfStudents = Comparator.comparing(Course::noOfStudents);
+        // not sure why it was not working with lambda function
+        Comparator<Course> comparingByNoOfStudentsDescending = Comparator.comparing(Course::noOfStudents).reversed();
+
+        // compare by no of students and no of reviews
+        // this comparator will put in reverse order of noOfStudents but then again reverse order ( i.e ascending order of reviewScore)
+        // [Course[name=API, category=Microservices, reviewScore=97, noOfStudents=22000],
+        // Course[name=AWS, category=Cloud, reviewScore=92, noOfStudents=21000], // REVIEW SCORE IS IN ASCENDING ORDER WHEN NO OF Students is same
+        // Course[name=Azure, category=Cloud, reviewScore=99, noOfStudents=21000]]
+        //  Comparator<Course> noOfStudentsAndReviews = Comparator.comparing(Course::noOfStudents).thenComparing(Comparator.comparing(Course::reviewScore).reversed()).reversed();
+
+        // [Course[name=API, category=Microservices, reviewScore=97, noOfStudents=22000],
+        // Course[name=Azure, category=Cloud, reviewScore=99, noOfStudents=21000], // HERE REVIEW SCORE IS ALSO ACCORDING TO DESCENDING ORDER when noOfStudents is same
+        // Course[name=AWS, category=Cloud, reviewScore=92, noOfStudents=21000]]
+        Comparator<Course> noOfStudentsAndReviews = Comparator.comparing(Course::noOfStudents).thenComparing(Course::reviewScore).reversed();
+
+        System.out.println(courses.stream().sorted(noOfStudentsAndReviews).filter(course -> course.noOfStudents >= 21000).collect(Collectors.toList()));
     }
 
     private static Predicate<Course> reviewScoreGreaterThanNinetyFive() {
