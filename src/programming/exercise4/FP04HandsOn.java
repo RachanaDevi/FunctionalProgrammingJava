9b20a47fc75d2399456fb6df0d7fbe6bc5151ca7
Rachana Devi <rachanadevi18@gmail.com>
2023-09-09 19:19:22 +0530

    Rachana | FP04 | Optimize comparator by using comparingInt as this will just direcly be comparing primitive values instead of inboxing and outboxing wrapper classes

--- src/programming/exercise4/FP04HandsOn.java
+++ src/programming/exercise4/FP04HandsOn.java
@@ -35,9 +35,9 @@ public class FP04HandsOn {
         System.out.println(courses.stream().anyMatch(reviewScoreGreaterThanNinety()));
 
         // sort courses by number of students
-        Comparator<Course> comparingByNoOfStudents = Comparator.comparing(Course::noOfStudents);
+        Comparator<Course> comparingByNoOfStudents = Comparator.comparingInt(Course::noOfStudents);
         // not sure why it was not working with lambda function
-        Comparator<Course> comparingByNoOfStudentsDescending = Comparator.comparing(Course::noOfStudents).reversed();
+        Comparator<Course> comparingByNoOfStudentsDescending = Comparator.comparingInt(Course::noOfStudents).reversed();
 
         // compare by no of students and no of reviews
         // this comparator will put in reverse order of noOfStudents but then again reverse order ( i.e ascending order of reviewScore)
@@ -49,7 +49,7 @@ public class FP04HandsOn {
         // [Course[name=API, category=Microservices, reviewScore=97, noOfStudents=22000],
         // Course[name=Azure, category=Cloud, reviewScore=99, noOfStudents=21000], // HERE REVIEW SCORE IS ALSO ACCORDING TO DESCENDING ORDER when noOfStudents is same
         // Course[name=AWS, category=Cloud, reviewScore=92, noOfStudents=21000]]
-        Comparator<Course> noOfStudentsAndReviews = Comparator.comparing(Course::noOfStudents).thenComparing(Course::reviewScore).reversed();
+        Comparator<Course> noOfStudentsAndReviews = Comparator.comparingInt(Course::noOfStudents).thenComparing(Course::reviewScore).reversed();
 
         System.out.println(courses.stream().sorted(noOfStudentsAndReviews).filter(course -> course.noOfStudents >= 21000).collect(Collectors.toList()));
     }
